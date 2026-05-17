6ecb791181400cfca1717f618b01be414a6cfb7e
ijei9 <ijei5042@gmail.com>
2026-05-17 15:15:20 +0900

    Add files via upload

---
+++ bot.py
@@ -0,0 +1,12 @@
+import discord
+import os
+
+intents = discord.Intents.default()
+client = discord.Client(intents=intents)
+
+@client.event
+async def on_ready():
+    print(f"Logged in as {client.user}")
+
+TOKEN = os.getenv("DISCORD_TOKEN")
+client.run(MTUwNTA4MzA3NDI4NTc5NzQyNg.G5IMlv.zqEjYasjnOBO6JlUjD1FRTdtcCSa2Sz7WWXXto)
